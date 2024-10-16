ec5ccd9acec3858362398a9bf3f2eb73f9315c67
Salocin0 <nicolasarieldurelli@gmail.com>
2024-10-16 19:53:22 -0300

    add filters

--- src/Producto.jsx
+++ src/Producto.jsx
@@ -1,9 +1,20 @@
-import "./Product.css"
+import "./Product.css";
 const Producto = (product) => {
-  console.log(product)
-  return (<div className="card">
-    <h2>{product.product.title}</h2>
-  </div>);
+  console.log(product);
+  return (
+    <div className="card">
+      <img
+        className="img"
+        src={product.product.image}
+        alt={product.product.description}
+      />
+      <div className="contenido">
+        <h2 className="titulo">{product.product.title}</h2>
+
+        <p>{product.product.price}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Producto;

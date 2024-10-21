548fcb9ac49a2f01f3db8a989e37f6e21c963cb1
Ivan Santander <isantander@gmail.com>
2024-10-21 19:33:37 -0300

    agregué loading

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ProductCardWithSale from './components/ProductCardWithSale';
 const App = () => {
   const [categories, setCategories] = useState([]);  
   const [products, setProducts] = useState([]);      
+  const [isLoading, setLoading] = useState(false);
 
   // Carga categorías
   useEffect(() => {
@@ -15,6 +16,7 @@ const App = () => {
       const response = await fetch('https://fakestoreapi.com/products/categories');
       const data = await response.json();
       setCategories(data);
+      setLoading(true);
     };
     fetchCategories();
   }, []);
@@ -27,18 +29,23 @@ const App = () => {
     setProducts(data);
   };
 
-  return (
+    if(!isLoading) {
+        return(<h1>Cargando...</h1>);
+    }else{
+     return (
     <div>
-      <h1>Tienda de Productos FakeStore</h1>
-      <Menu categories={categories} onCategorySelect={handleCategorySelect} />
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
-        {products.map(product => (
-          //<ProductCard key={product.id} product={product} />
-          <ProductCardWithSale key={product.id} product={product} />
-        ))}
-      </div>
+            <h1>Tienda de Productos FakeStore</h1>
+            <Menu categories={categories} onCategorySelect={handleCategorySelect} />
+            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+              {products.map(product => (
+                //<ProductCard key={product.id} product={product} />
+                <ProductCardWithSale key={product.id} product={product} />
+              ))}
+            </div>
     </div>
-  );
+    );
+    }
+
 };
 
 export default App;
